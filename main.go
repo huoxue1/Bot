71ea4935bccfb4fdf1012f5bf3ad72f5bf84a3bb
1743224847gou <3343780376@qq.com>
2021-02-03 11:35:50 +0800

    commit

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func handHttp(engine *gin.Engine) {
 	engine.POST("/hook", func(context *gin.Context) {
 		date, _ := ioutil.ReadAll(context.Request.Body)
 		if strings.Contains(context.Request.Header.Get("User-Agent"), "GitHub") &&
-			strings.Contains(string(date), `"ref": "refs/heads/master"`) {
+			strings.Contains(string(date), `"ref": "refs/heads/dev"`) {
 			log.Println("开始执行pull ")
 			command := exec.Command("/bin/sh", "-c", "git pull https://github.com/3343780376/Bot")
 			err := command.Start()
