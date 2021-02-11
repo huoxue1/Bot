1c1575857b26fa200fbd2c529e304140b469f325
1743224847gou <3343780376@qq.com>
2021-02-11 21:56:39 +0800

    commit

--- plugins/All/all.go
+++ plugins/All/all.go
@@ -215,13 +215,17 @@ func downloadFile(fileName string, url string) {
 
 //除夕快乐
 func NewYear(event go_mybots.Event, args []string) {
-	fmt.Println("出发了事件")
+	if event.UserId == 3343780376 {
+		return
+	}
+	fmt.Println(time.Now().Month())
+	fmt.Println(time.Now().Day())
 	if (event.GroupId == 727429388 || event.GroupId == 17185204) && time.Now().Month() == 2 && time.Now().Day() == 12 {
 		rand.Seed(time.Now().UnixNano())
 		n := rand.Intn(5)
 		connect := model.DbInit()
 		connect.Update(n, event)
-		bot.SendGroupMsg(event.GroupId, "新年快乐，恭喜你获得随机积分"+strconv.Itoa(n)+"个"+go_mybots.MessageAt(event.UserId).Message, false)
+		bot.SendGroupMsg(event.GroupId, "小伙伴新年快乐，恭喜你获得随机积分"+strconv.Itoa(n)+"个"+go_mybots.MessageAt(event.UserId).Message+",祝你在牛年天天开心", false)
 	} else if event.GroupId == 972264701 {
 		rand.Seed(time.Now().UnixNano())
 		n := rand.Intn(5)
